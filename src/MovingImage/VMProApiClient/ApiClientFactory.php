a35c6eb74add8a0a51427d319b0d9e9c258a0abf
Ruben Knol <ruben.knol@movingimage.com>
2016-11-14 08:39:46 +0100

    New file

---
+++ src/MovingImage/VMProApiClient/ApiClientFactory.php
@@ -0,0 +1,21 @@
+<?php
+namespace MovingImage\VMProApiClient;
+
+/**
+ * Class ApiClientFactory
+ *
+ * @author Ruben Knol <ruben.knol@movingimage.com>
+ * @package MovingImage\VMProApiClient
+ */
+class ApiClientFactory
+{
+    public function __construct()
+    {
+
+    }
+
+    public function create()
+    {
+        return new ApiClient();
+    }
+}
