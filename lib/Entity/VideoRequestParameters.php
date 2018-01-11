453000376a61c88ff051e2ee0e92a51dd5ac0af6
Pavle Predic <pavle.predic@gmail.com>
2018-01-11 16:09:50 +0100

    Cache access token (if a CacheItemPool is provided to the TokenManager)

--- lib/Entity/VideoRequestParameters.php
+++ lib/Entity/VideoRequestParameters.php
@@ -7,15 +7,15 @@ use MovingImage\Client\VMPro\Util\AccessorTrait;
 /**
  * Class VideosRequestParameters.
  *
- * @method bool isIncludeCustomMetadata()
+ * @method bool                   isIncludeCustomMetadata()
  * @method VideoRequestParameters setIncludeCustomMetadata(bool $includeCustomMetadata)
- * @method string getCustomMetadataField()
+ * @method string                 getCustomMetadataField()
  * @method VideoRequestParameters setCustomMetadataField(string $customMetadataField)
- * @method bool isIncludeKeywords()
+ * @method bool                   isIncludeKeywords()
  * @method VideoRequestParameters setIncludeKeywords(bool $includeKeywords)
- * @method int getRelatedVideosChannelId()
+ * @method int                    getRelatedVideosChannelId()
  * @method VideoRequestParameters setRelatedVideosChannelId(int $relatedVideosChannelId)
- * @method bool isIgnorePublicationState()
+ * @method bool                   isIgnorePublicationState()
  * @method VideoRequestParameters setIgnorePublicationState(bool $ignorePublicationState)
  *
  * @author Robert Szeker <robert.szeker@movingimage.com>
