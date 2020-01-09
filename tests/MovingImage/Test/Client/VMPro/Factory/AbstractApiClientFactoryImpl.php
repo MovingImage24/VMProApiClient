85c21e5794b37e0a5e4f75968b07f62c96ac8b1e
Constantin Plotkin <kudrikkk@yandex.ru>
2020-01-09 16:57:45 +0100

    fix bug

--- tests/MovingImage/Test/Client/VMPro/Factory/AbstractApiClientFactoryImpl.php
+++ tests/MovingImage/Test/Client/VMPro/Factory/AbstractApiClientFactoryImpl.php
@@ -4,6 +4,7 @@ namespace MovingImage\Test\Client\VMPro\Factory;
 
 use MovingImage\Client\VMPro\Entity\ApiCredentials;
 use MovingImage\Client\VMPro\Factory\AbstractApiClientFactory;
+use MovingImage\Client\VMPro\Interfaces\ApiClientInterface;
 use MovingImage\Test\Client\VMPro\ApiClient\AbstractApiClientImpl;
 
 class AbstractApiClientFactoryImpl extends AbstractApiClientFactory
@@ -21,7 +22,7 @@ class AbstractApiClientFactoryImpl extends AbstractApiClientFactory
         return 'base_uri';
     }
 
-    public function createSimple($baseUri, ApiCredentials $credentials, $authUrl)
+    public function createSimple($baseUri, ApiCredentials $credentials, $authUrl): ApiClientInterface
     {
     }
 }
