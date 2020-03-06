936e1d52f72094f019288416f1df64b10def7a0c
robertszeker <robert.szeker@movingimage.com>
2020-03-06 08:33:23 +0100

    CT-1708 added endpoint for CorporateTube meta data (#84)

---
+++ lib/Entity/CorporateTubeMetaData.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MovingImage\Client\VMPro\Entity;
+
+use DateTime;
+use JMS\Serializer\Annotation\SerializedName;
+use JMS\Serializer\Annotation\Type;
+
+class CorporateTubeMetaData
+{
+    /**
+     * @Type("DateTime")
+     *
+     * @var DateTime
+     */
+    private $uploadDate;
+
+    /**
+     * @Type("string")
+     * @SerializedName("uploaderUserId")
+     *
+     * @var string
+     */
+    private $uploaderUserId;
+
+    /**
+     * @Type("string")
+     * @SerializedName("inChargeUserId")
+     *
+     * @var string
+     */
+    private $inChargeUserId;
+
+    public function getUploadDate(): DateTime
+    {
+        return $this->uploadDate;
+    }
+
+    public function setUploadDate(DateTime $uploadDate): self
+    {
+        $this->uploadDate = $uploadDate;
+
+        return $this;
+    }
+
+    public function getUploaderUserId(): string
+    {
+        return $this->uploaderUserId;
+    }
+
+    public function setUploaderUserId(string $uploaderUserId): self
+    {
+        $this->uploaderUserId = $uploaderUserId;
+
+        return $this;
+    }
+
+    public function getInChargeUserId(): string
+    {
+        return $this->inChargeUserId;
+    }
+
+    public function setInChargeUserId(string $inChargeUserId): self
+    {
+        $this->inChargeUserId = $inChargeUserId;
+
+        return $this;
+    }
+}
